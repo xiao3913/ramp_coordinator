4cd3c4c0b4e4ea3406671bdb0b9281c35b0be629
xiao3913 <xiao2@kth.se>
2021-03-08 21:45:19 +0100

    Add GetOptimal method to StateClass for calculation of forward DP

--- StateClass.m
+++ StateClass.m
@@ -9,5 +9,25 @@ classdef StateClass < handle
         function obj = StateClass(val)
             obj.state = val;
         end
+        function obj = GetOptimal(obj, t_min, tau, tau_L)
+            obj.optimal_path = 0;
+            obj.optimal_value = 1000;
+            for i = 1:length(obj.prev_states)
+                optimal_to_come = obj.prev_states(i).optimal_value;
+                if obj.state(3) == obj.prev_states(i).state(3)
+                    value = max(t_min, optimal_to_come + tau);
+                else
+                    if obj.prev_states(i).state(3) == 0
+                        value = max(t_min, optimal_to_come);
+                    else
+                        value = max(t_min, optimal_to_come + tau_L);
+                    end
+                end
+            end
+            if value < opt_val
+                obj.optimal_value = value;
+                obj.optimal_path = k;
+            end
+        end
     end
 end
