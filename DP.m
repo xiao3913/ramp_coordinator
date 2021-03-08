ecf41ff5416ae3e08d31e5c4b33aa05ef53b8af8
xiao3913 <xiao2@kth.se>
2021-03-08 17:56:06 +0100

    Add stateclass object for the representation of state in the DP tree, and add state transition to the DP tree

--- DP.m
+++ DP.m
@@ -9,18 +9,56 @@ function state_tree = DP(n, m)
 %   transition between states
 
 state_tree{n+m+1} = [];
-state_tree{1} = [0 0 0];
+state_tree{1} = StateClass([0 0 0]);
 for i = 1:n+m
     lower = max(0,i-n);
     upper = min(i,n);
+    index = 0;
     for j = lower:upper
         if j == 0
-            state_tree{i+1} = [state_tree{i+1}; [j i-j 2]];
+            index = index + 1;
+            state_tree{i+1} = [state_tree{i+1}; StateClass([j i-j 2])];
+            if i-j-1 == 0
+                prev_state = [0 0 0];
+                state_tree{i+1}(index,:).prev_states = findobj(state_tree{i},'state',prev_state);
+            else
+                prev_state = [j i-j-1 2];
+                state_tree{i+1}(index,:).prev_states = findobj(state_tree{i},'state',prev_state);
+            end
         elseif i-j == 0
-            state_tree{i+1} = [state_tree{i+1}; [j i-j 1]];
+            index = index + 1;
+            state_tree{i+1} = [state_tree{i+1}; StateClass([j i-j 1])];
+            if j-1 == 0
+                prev_state = [0 0 0];
+                state_tree{i+1}(index,:).prev_states = findobj(state_tree{i},'state',prev_state);
+            else
+                prev_state = [j-1 i-j 1];
+                state_tree{i+1}(index,:).prev_states = findobj(state_tree{i},'state',prev_state);
+            end
         else
-            state_tree{i+1} = [state_tree{i+1}; [j i-j 1]];
-            state_tree{i+1} = [state_tree{i+1}; [j i-j 2]];
+            index = index + 1;
+            state_tree{i+1} = [state_tree{i+1}; StateClass([j i-j 1])];
+            if j-1 == 0
+                prev_state = [j-1 i-j 2];
+                state_tree{i+1}(index,:).prev_states = findobj(state_tree{i},'state',prev_state);
+            else
+                prev_state = [j-1 i-j 1];
+                state_tree{i+1}(index,:).prev_states = findobj(state_tree{i},'state',prev_state);
+                prev_state = [j-1 i-j 2];
+                state_tree{i+1}(index,:).prev_states = [state_tree{i+1}(index,:).prev_states; findobj(state_tree{i},'state',prev_state)];
+            end
+            
+            index = index + 1;
+            state_tree{i+1} = [state_tree{i+1}; StateClass([j i-j 2])];
+            if i-j-1 == 0
+                prev_state = [j i-j-1 1];
+                state_tree{i+1}(index,:).prev_states = findobj(state_tree{i},'state',prev_state);
+            else
+                prev_state = [j i-j-1 1];
+                state_tree{i+1}(index,:).prev_states = findobj(state_tree{i},'state',prev_state);
+                prev_state = [j i-j-1 2];
+                state_tree{i+1}(index,:).prev_states = [state_tree{i+1}(index,:).prev_states; findobj(state_tree{i},'state',prev_state)];
+            end
         end
     end
 end
