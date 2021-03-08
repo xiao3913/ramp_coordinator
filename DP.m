b8e04fe93617b63b78605e23fb6bcff28ad3364d
xiao3913 <xiao2@kth.se>
2021-03-08 16:44:23 +0100

    Change DP to a function format and add explanatory comments for the use of function

--- DP.m
+++ DP.m
@@ -1,6 +1,12 @@
-clear all
-m = 3;
-n = 3;
+function state_tree = DP(n, m)
+% DP.m is the implementation of Dynamic Programming algorithm
+% Inputs:
+%   n : number of mainroad vehicles
+%   m : number of ramp vehicles
+%
+% Output:
+%   state_tree : the tree structure representing states at each stage and
+%   transition between states
 
 state_tree{n+m+1} = [];
 state_tree{1} = [0 0 0];
@@ -17,4 +23,6 @@ for i = 1:n+m
             state_tree{i+1} = [state_tree{i+1}; [j i-j 2]];
         end
     end
+end
+
 end
