9fa887c37310fa36dbc68d97efe4716a28de379a
xiao3913 <xiao2@kth.se>
2021-03-10 19:14:58 +0100

    Testing: add more test cases to InitializeVehicleTest.m

--- test/InitializeVehicleTest.m
+++ test/InitializeVehicleTest.m
@@ -36,5 +36,21 @@ classdef InitializeVehicleTest < matlab.unittest.TestCase
             testCase.verifyLessThanOrEqual(max_vehicle_pos, CR_length)
             testCase.verifyGreaterThanOrEqual(min_vehicle_pos, 0)
         end
+        function testTimeOrder(testCase)
+            % test that the times are sorted in ascend order
+            v_max = 20; a_max = 3; CR_length = 300; tau = 1; num = 6;
+            [~, ~, time] = initialize_vehicle(num, v_max, a_max, CR_length, tau);
+            for i = 2:length(time)
+                testCase.verifyLessThan(time(i-1),time(i))
+            end
+        end
+        function testPositionOrder(testCase)
+            % test that the postions are sorted in descend order
+            v_max = 20; a_max = 3; CR_length = 300; tau = 1; num = 6;
+            [pos, ~, ~] = initialize_vehicle(num, v_max, a_max, CR_length, tau);
+            for i = 2:length(pos)
+                testCase.verifyLessThan(pos(i),pos(i-1))
+            end
+        end
     end
 end
