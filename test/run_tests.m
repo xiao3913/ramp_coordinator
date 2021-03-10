0c514035036485843ee874922ed98acf1d4804ea
xiao3913 <xiao2@kth.se>
2021-03-10 19:22:45 +0100

    add all current tests into run_tests.m

--- test/run_tests.m
+++ test/run_tests.m
@@ -1,6 +1,8 @@
 import matlab.unittest.TestSuite
 suite1 = TestSuite.fromFile('DPtest.m');
 suite2 = TestSuite.fromFile('ReachableSetTest.m');
-totalSuite = [suite2];
+suite3 = TestSuite.fromFile('StateClassTest.m');
+suite4 = TestSuite.fromFile('InitializeVehicleTest.m');
+totalSuite = [suite2 suite3 suite3];
 totalResult = run(totalSuite);
 disp(totalResult)
