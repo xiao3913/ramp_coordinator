ab627c0bc36e78a3b289e2ca60ba429443123a58
xiao3913 <xiao2@kth.se>
2021-03-10 18:33:15 +0100

    Modify main.m so it can be used to generate a randomized scenario and solve the coordination by calling DP

--- main.m
+++ main.m
@@ -1,5 +1,23 @@
+%% User defined parameters
+v_max = 25; %maximal allowed velocity [m/s] choose between [10 25]
+v_min = 0;  %minimal allowed velocity [m/s] choose between [0 v_max]
+a_max = 3;  %maximal allowed acceleration [m/s^2] choose between [1.5 5]
+a_min = -3; %minimal allowed acceleration [m/s^2] choose between [-5 -1.5]
+
+CR_length = 300; %define the length of the control region [m] 300 is reasonable
+n = 7; %number of main road vehicles
+m = 4; %number of ramp vehicles
+
+tau = 1; tau_L = 1.5; %specify time headway
+%% Initialize the scenario at random given the user parameters
+
+[pos_n, vel_n, time_n] = initialize_vehicle(n, v_max, a_max, CR_length, tau); %initialize vehicles on main road
+
+[pos_m, vel_m, time_m] = initialize_vehicle(m, v_max, a_max, CR_length, tau); %initialize vehcile on ramp
+
+%% Running the optimization and display results
 tic 
-[state_tree, t_arrival_n, t_arrival_m] = DP(n, m, t_min_n, t_min_m, tau, tau_L);
+[state_tree, t_arrival_n, t_arrival_m] = DP(n, m, time_n, time_m, tau, tau_L);
 toc
 
 plot(t_arrival_n,'*')
@@ -9,4 +27,4 @@ xlabel('vehicles')
 ylabel('arrival time')
 legend('main road', 'on ramp')
 total_arrival_time = sum(t_arrival_n) + sum(t_arrival_m)
-total_delay = sum(t_arrival_n - t_min_n) + sum(t_arrival_m - t_min_m)
+total_delay = sum(t_arrival_n - time_n) + sum(t_arrival_m - time_m)
